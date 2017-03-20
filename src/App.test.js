f90b24da90694dae0bd955ba05ba654697062224
Brittany Storoz <brittanystoroz@gmail.com>
2017-03-20 13:41:09 -0400

    Cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import fetchMock from 'fetch-mock';
 
 import App from './App';
-import AddGroceryForm from './AddGroceryForm';
-import GroceryList from './GroceryList';
-import Grocery from './Grocery';
 
 describe('App', () => {
-
-  const mockGroceries = [
-    { id: 1489863729151, name: 'Pineapples', quantity: 10, purchased: false, starred: false },
-    { id: 1489863740047, name: 'Coconuts', quantity: 1000, purchased: false, starred: false },
-  ];
 
   afterEach(() => {
     expect(fetchMock.calls().unmatched).toEqual([]);
