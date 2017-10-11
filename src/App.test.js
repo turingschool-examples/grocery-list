ea939a8329c916048cff921fecdd5dfe4721b554
Mike Limberg <limbergmike@gmail.com>
2017-10-11 11:31:46 -0600

    Final Lesson code

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,11 @@ import fetchMock from 'fetch-mock';
 
 describe('App', () => {
 
+  afterEach(() => {
+    expect(fetchMock.calls().unmatched).toEqual([]);
+    fetchMock.restore();
+});
+
   const mockData = {
     groceries: [
       { id: 1489863729151, name: 'Pizza', quantity: 10, purchased: false, starred: false },
@@ -32,7 +37,6 @@ describe('App', () => {
 
     await pause()
 
-    console.log(wrapper.debug())
     const groceriesOnDOM = wrapper.find('Grocery')
 
     expect(wrapper.state().groceries).toEqual(mockData.groceries)
@@ -40,37 +44,74 @@ describe('App', () => {
   })
 
   it('submits the correct data when adding a new grocery', async () => {
-  fetchMock.post('/api/v1/groceries', {
-    status: 200,
-    body: mockData
+    fetchMock.post('/api/v1/groceries', {
+      status: 200,
+      body: mockData
+    });
+
+    const mockFn = jest.fn()
+
+    const wrapper = mount(<AddGroceryForm updateGroceryList={ mockFn } />)
+
+    const nameInput = wrapper.find('input[name="name"]');
+    const qtyInput = wrapper.find('input[name="quantity"]');
+    const formElem = wrapper.find('form');
+
+    nameInput.simulate('change', {
+      target: { name: 'name', value: 'Foo' }
+    });
+
+    qtyInput.simulate('change', {
+      target: { name: 'quantity', value: '1000' }
+    });
+
+    formElem.simulate('submit');
+
+    await pause()
+
+    expect(mockFn).toHaveBeenCalledTimes(1)
   });
 
-  const mockFn = jest.fn()
+  it('submits the correct data when adding a new grocery', async () => {
+    const mockGroceries = [
+      { id: 1489863729151, name: 'Pizza', quantity: 10, purchased: false, starred: false },
+      { id: 1489863740047, name: 'Steak', quantity: 1000, purchased: false, starred: false },
+      { id: 1489863740048, name: 'Toaster Strudel', quantity: 10000, purchased: false, starred: false },
+      { id: 1489863740049, name: 'Ice Cream', quantity: 10000, purchased: false, starred: false },
+    ]
 
-  const wrapper = mount(<AddGroceryForm updateGroceryList={ mockFn } />)
+    fetchMock.get('/api/v1/groceries', {
+      status: 200,
+      body: mockData
+    })
 
-  const nameInput = wrapper.find('input[name="name"]');
-  const qtyInput = wrapper.find('input[name="quantity"]');
-  const formElem = wrapper.find('form');
+    fetchMock.post('/api/v1/groceries', {
+      status: 200,
+      body: mockGroceries
+    });
 
-  nameInput.simulate('change', {
-    target: { name: 'name', value: 'Foo' }
+
+    const wrapper = mount(<App />)
+
+    await pause()
+
+
+    const nameInput = wrapper.find('input[name="name"]');
+    const qtyInput = wrapper.find('input[name="quantity"]');
+    const formElem = wrapper.find('form');
+
+    nameInput.simulate('change', {
+      target: { name: 'name', value: 'Foo' }
+    });
+
+    qtyInput.simulate('change', {
+      target: { name: 'quantity', value: '1000' }
+    });
+
+    formElem.simulate('submit');
+
+    await pause()
+
+    expect(wrapper.state().groceries).toEqual(mockGroceries)
   });
-
-  qtyInput.simulate('change', {
-    target: { name: 'quantity', value: '1000' }
-  });
-
-  formElem.simulate('submit');
-
-  await pause()
-
-  expect(mockFn).toHaveBeenCalledTimes(1)
-});
-
-
-
-
-
-
 })
